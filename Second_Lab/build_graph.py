6e498eb57b59855c99534fc9396cf97f1b137ad7
AvtoBBus <113329450+AvtoBBus@users.noreply.github.com>
2023-05-14 23:13:18 +0400

    some fix
    Небольшие изменения в коде, построения графиков

--- Second_Lab/build_graph.py
+++ Second_Lab/build_graph.py
@@ -8,9 +8,23 @@ def cm_to_inch(value):
 
 def start_create_graph(file_path_list: list):
 
-    list_num_of_node = [[], [], [], [], [], []]
-    list_comparison = [[], [], [], [], [], []]
-    list_copy = [[], [], [], [], [], []]
+    list_num_of_node = [[]for i in range(6)]
+    list_comparison = [[] for i in range(6)]
+    list_copy = [[] for i in range(6)]
+    list_text = [
+        "Random_change_comp",
+        "Random_change_copy",
+        "Random_shell_comp",
+        "Random_shell_copy",
+        "Sorted_change_comp",
+        "Sorted_change_copy",
+        "Sorted_shell_comp",
+        "Sorted_shell_copy",
+        "Reversed_change_comp",
+        "Reversed_change_copy",
+        "Reversed_shell_comp",
+        "Reversed_shell_copy",
+    ]
     for j in range(len(file_path_list)):
         help_list = []
 
@@ -29,19 +43,22 @@ def start_create_graph(file_path_list: list):
         "left": 0.040,
         "bottom": 0.060,
         "right": 0.990,
-        "top": 0.990
+        "top": 0.950
     }
     figure.subplots_adjust(**margins)
-
+    
     for i in range(1, 13):
         plt.subplot(6, 2, i)
         plt.xlabel("Number of node")
+        plt.grid(True)
         if i % 2 == 1:
             plt.ylabel("Comp count")
             plt.plot(list_num_of_node[count], list_comparison[count])
+            plt.text(0, list_comparison[count][-1] / 2, list_text[i-1])
         else:
             plt.ylabel("Copy count")
             plt.plot(list_num_of_node[count], list_copy[count])
+            plt.text(0, list_copy[count][-1] / 2, list_text[i-1])
             count += 1
 
     plt.show()
